d7091575570f16350104e80b45d43ea7209b9ec6
DARIOVRUNA <ramon.vargas.rojas@est.una.ac.cr>
2023-03-20 00:10:29 -0600

    html,css, js metodos

---
+++ js/temperature-calculator.js
@@ -0,0 +1,6 @@
+let from_Kelvin_To_Celcius =  (k) =>  k - 273.15
+let from_Fahrenheit_To_Celcius = (f) => 5*(f - 32 )/9
+let from_Celcius_To_Kelvin = (c) => c + 273.15
+let from_Kelvin_To_Fahrenheit = (k) => 9*(k - 273.15) / 5 + 32
+let from_Fahrenheit_To_Kelvin = (f) => 5*(f - 32) / 9 + 273.15
+let from_Celcius_To_Fahrenheit = (c) => 9*c/ 5 + 32
